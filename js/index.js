9ed8935050a0fb87b47c0d9f5170e05529755f9d
AhmadjonNoyibjonov <ahmadjonnoyibjonov60@gmail.com>
2024-05-20 18:31:59 +0500

    project finished

---
+++ js/index.js
@@ -0,0 +1,239 @@
+// 1-masala
+// let n = +prompt("n soni kiritilsin");
+// if (n > 0) {
+//     n++;
+// } else {n
+// }
+// console.log(n);
+
+// 2-masala
+// let n = +prompt("n soni kiritilsin");
+// if (n > 0) {
+//     n++;
+// } else if (n < 0) {
+//     n -=2;
+// }
+// console.log(n);
+
+// 3-masala
+// let n = +prompt("n soni kiritilsin");
+// if (n > 0) {
+//     n++;
+// } else if (n=0) {
+//     n = 10;
+// } else if (n < 0) {
+//     n -=2;
+// }
+// console.log(n);
+
+// 4-masala
+// let a = +prompt("a soni kiritilsin");
+// let b = +prompt("b sonini kiriting");
+// let c = +prompt("c sonini kiriting");
+// let musbat = 0;
+// if (a > 0) {
+//   musbat = musbat + 1;
+// }
+// if (b > 0) {
+//   musbat = musbat + 1;
+// }
+// if (c > 0) {
+//   musbat = musbat + 1;
+// }
+// console.log(musbat);
+
+// 5- masala
+// let a = +prompt("a soni kiritilsin");
+// let b = +prompt("b sonini kiriting");
+// let c = +prompt("c sonini kiriting");
+// let d = +prompt("d sonini kiriting");
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (a > 0) {
+//   musbat = musbat + 1;
+// } else  if (a < 0) {
+//   manfiy = manfiy + 1;
+// }
+
+// if (b % 2 == 1) {
+//   musbat = musbat + 1;
+// } else  if (b < 0){
+//   manfiy = manfiy + 1;
+// }
+
+// if (c % 2 == 1) {
+//   musbat = musbat + 1;
+// } else  if (c < 0){
+//   manfiy = manfiy + 1;
+// }
+
+// if (d % 2 == 1) {
+//   musbat = musbat + 1;
+// } else if (d < 0) {
+//   manfiy = manfiy + 1;
+// }
+
+// console.log(manfiy);
+// console.log(musbat);
+
+// 6-masala
+// let a = +prompt("a soni kiritilsin");
+// let b = +prompt("b soni kiritilsin");
+// let max = 0;
+
+// if (a > b) {
+//     max = a;
+// } else {
+//     max = b
+// }
+
+// 7-masala
+// let a = +prompt("a soni kiritilsin");
+// let b = +prompt("b soni kiritilsin");
+// let max;
+// if (a > b) {
+//     max=1;
+// } else {
+//     max=2
+// }
+
+// 8-masala
+// let a = +prompt("a soni kiritilsin");
+// let b = +prompt("b soni kiritilsin");
+
+// let max = 0;
+// let min = 0;
+
+// if (a > b) {
+//   max = max + a;
+//   min = min + b;
+// } else if (b > a) {
+//   max = max + b;
+//   min = min + a;
+// }
+
+// console.log(max);
+// console.log(min);
+
+// 9-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kiritilsin");
+// let c;
+// if (A>B) {
+//     c = B;
+//     B = A;
+//     A = c;
+// }
+
+// 10-masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kiritilsin");
+// let max;
+// if (A == B) {
+//   A = 0;
+//   B = 0;
+// } else if (A > B) {
+//   max = A;
+//   A = A;
+//   B = A;
+// } else if (B > A) {
+//   max = B;
+//   B = B;
+//   A = B;
+// }
+
+// 11- masala
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kiritilsin");
+
+// if (A == B) {
+//     A = 0;
+//     B = 0;
+// } else if (A > B) {
+//     A = A;
+//     B =A;
+// } else if (B > A) {
+//     A = B;
+//     B = B;
+// }
+
+// 12- masala
+// let a = +prompt("a soni kiritilsin");
+// let b = +prompt("b soni kiritilsin");
+// let c = +prompt("c soni kiritilsin");
+
+// let min;
+
+// if (a > b) {
+//     min = a;
+// } else {
+//     min = b
+// }
+
+// if (min > c) {
+//     min = c;
+// }
+
+// console.log(min);
+
+// 13-masala
+// let a = +prompt("a soni kiritilsin");
+// let b = +prompt("b soni kiritilsin");
+// let c = +prompt("c soni kiritilsin");
+// let mid;
+
+// if (a > b && a < c) {
+//     mid = a;
+// } else if ( b > a && b < c) {
+//     mid = b
+// } else if ( c > a && c < b) {
+//     mid = c;
+// }
+
+// console.log(mid);
+
+// 14-masala
+// let a = +prompt("a soni kiritilsin");
+// let b = +prompt("b soni kiritilsin");
+// let c = +prompt("c soni kiritilsin");
+// let min;
+// let max;
+// if (a < b && a < c) {
+//     min = a;
+// } else if (b < a && b < c) {
+//     min = b;
+// } else if (c < a && c < b) {
+//     min = c;
+// }
+
+// if (a > b && a > c) {
+//     max = a;
+// } else if (b > a && b > c) {
+//     max = b;
+// } else if (c > a && c > b) {
+//     max = c;
+// }
+
+// console.log(min);
+// console.log(max);
+
+// 15-masala
+// let a = +prompt("a soni kiritilsin");
+// let b = +prompt("b soni kiritilsin");
+// let c = +prompt("c soni kiritilsin");
+// let max1;
+// let max2;
+// let max = max1+max2
+// if (a > b && a > c && b > c) {
+//     max1 = a;
+//     max2 = b
+// } else if (b > a && b > c && c > a) {
+//     max1 = b;
+//     max2 = c;
+// } else if (c > a && c > b && a > b) {
+//     max1 = c;
+//     max2 = a
+// }
+
+// console.log(max);
